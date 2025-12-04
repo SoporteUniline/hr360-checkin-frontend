7e46ed49654330c27c4a9fa6b87617b5c09b5181
Luis Ángel <devjsluis@gmail.com>
2025-12-04 09:40:03 -0600

    feat: :sparkles: Button to contact by Whatsapp

--- src/components/ModalCapacidadAgotada.jsx
+++ src/components/ModalCapacidadAgotada.jsx
@@ -6,18 +6,33 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function ModalCapacidadAgotada({ open, onClose, mensaje }) {
+  const whatsappNumber = "5213171035768";
+  const texto = encodeURIComponent(
+    "Hola, necesito apoyo para mejorar mi plan en HR360. ¿Me podrían ayudar?"
+  );
+
+  const url = `https://wa.me/${whatsappNumber}?text=${texto}`;
+
   return (
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent className="max-w-md">
         <DialogHeader className="border-b-2 pb-4">
           <DialogTitle>No es posible crear un empleado</DialogTitle>
         </DialogHeader>
-        <p className="text-sm text-gray-700">
+
+        <p className="text-sm text-gray-700 mb-1">
           {mensaje} Te sugerimos ponerte en contacto con el administrador de
           HR360 para obtener un plan acorde a tus necesidades.
         </p>
+
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <Button className="w-full bg-white text-emerald-700 border-emerald-700 border-2 hover:bg-gray-200">
+            Contactar por WhatsApp
+          </Button>
+        </a>
       </DialogContent>
     </Dialog>
   );
