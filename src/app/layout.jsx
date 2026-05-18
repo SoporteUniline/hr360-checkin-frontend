fcc6465562c03abe2d74d022f817fd9739de9f62
LuisAngelGGG <devjsluis@gmail.com>
2026-05-18 16:01:29 -0600

    Pixel Facebook

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,17 +22,47 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        {/* Script de Google Maps sin event handlers */}
+
+        {/* Script de Google Maps */}
         <Script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
           strategy="afterInteractive"
         />
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2003925690497108');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
+
       <body
         className={`${inter.variable} antialiased`}
         style={{ fontFamily: "var(--font-inter)" }}
       >
         <ClientProviders>{children}</ClientProviders>
+
+        {/* Meta Pixel NoScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2003925690497108&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
